c212d6b8fc5fa3baacf2bc131de198b3291437b0
thesmartone <thesmartone0@gmail.com>
2023-05-12 15:37:13 -0400

    Service Registry and Discovery with Eureka Netflix

---
+++ service-registry/src/test/java/dev/codextended/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codextended.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
